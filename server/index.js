5d54781243c62d3ec9662d770a3eadab653b777d
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-06-13 15:45:26 +0530

    Add database connection test in index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import db from './config/db.js'
 
 dotenv.config()
 
@@ -8,6 +9,11 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+// Test DB connection
+db.getConnection()
+  .then(() => console.log('MySQL connected successfully!'))
+  .catch(err => console.error('MySQL connection error:', err))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Recycling Tracker API is running!' })
