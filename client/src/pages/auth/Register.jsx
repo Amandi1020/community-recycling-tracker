ddc0abc4d097582ecbac3b0e522dc0cf13ed7113
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-06-15 00:38:42 +0530

    Fix password field value binding in Register page

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -88,7 +88,7 @@ const Register = () => {
             <input
               type="password"
               name="password"
-              value={formData.email}
+              value={formData.password}
               onChange={handleChange}
               required
               className="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-green-400"
