9994712857039a3323d6d10f6d295cc63b1c0030
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-06-18 20:21:00 +0530

    Connect admin routes to Express app in index.js

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import db from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import residentRoutes from './routes/residentRoutes.js'
 import collectorRoutes from './routes/collectorRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
 
 dotenv.config()
 
@@ -19,6 +20,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
+
 db.getConnection()
   .then(() => console.log('MySQL connected successfully!'))
   .catch(err => console.error('MySQL connection error:', err))
@@ -26,6 +28,7 @@ db.getConnection()
 app.use('/api/auth', authRoutes)
 app.use('/api/resident', residentRoutes)
 app.use('/api/collector', collectorRoutes)
+app.use('/api/admin', adminRoutes)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Recycling Tracker API is running!' })
