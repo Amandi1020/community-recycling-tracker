e9f732b2e5e053d8f237b332acfa4e680741e39e
Amandi1020 <amandirajapaksha2003@gmail.com>
2026-06-29 15:02:32 +0530

    Add admin routes for user status, delete and category management

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { getDashboard, getUsers } from '../controllers/adminController.js'
+import { getDashboard, getUsers, updateUserStatus, deleteUser, getCategories, updateCategory } from '../controllers/adminController.js'
 import verifyToken from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.get('/dashboard', verifyToken, getDashboard)
 router.get('/users', verifyToken, getUsers)
+router.put('/users/:userId/status', verifyToken, updateUserStatus)
+router.delete('/users/:userId', verifyToken, deleteUser)
+router.get('/categories', verifyToken, getCategories)
+router.put('/categories/:categoryId', verifyToken, updateCategory)
 
 export default router
